ae6e49442c89bb0008a55a7cfa2d4e631da8ae8d
Jesus Arambula <61527697+JesusEArambula@users.noreply.github.com>
2021-08-19 15:39:48 -0700

    i18n Translation Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Suspense} from 'react';
+import {useTranslation } from 'react-i18next';
+
+
+function HeaderComponent()
+{
+  const [t, i18n] = useTranslation('common');
+  return(
+    <div>
+      <h1>{t('welcome.title', {framework: 'React'})}</h1>
+      <button onClick={() => i18n.changeLanguage('en')}>English</button>
+      <button onClick={() => i18n.changeLanguage('es')}>Spanish</button>
+    </div>
+  );
+}
 
 function App() {
   return (
+    <Suspense fallback = "loading">
     <div className="App">
+      <HeaderComponent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +36,20 @@ function App() {
         </a>
       </header>
     </div>
+    </Suspense>
   );
 }
+
+
+// class HigherOrderComponent extends Component {
+//   render() 
+//   {
+//     const { t } = this.props;
+//     return <h1>{t('welcome.title', {framework: 'React'})}</h1>
+//   }
+// }
+// const HigherOrderComponentTranslated = withTranslation('common')(HigherOrderComponent);
+
+
 
 export default App;
